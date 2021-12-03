eea1e6d877efec2784c9c9a9626c30cebd7fe107
selvakumar15 <selvakumar.ramasamyk@gmail.com>
2021-12-04 00:55:10 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+ 
+		int count = 7, num1 = 0, num2 = 1;
+        System.out.print("Fibonacci Series of "+count+" numbers:");
+
+        for (int i = 1; i <= count; ++i)
+        {
+            System.out.print(num1+" ");
+
+ 
+            int sumOfPrevTwo = num1 + num2;
+            num1 = num2;
+            num2 = sumOfPrevTwo;
+		
+	}
+	}
+}
